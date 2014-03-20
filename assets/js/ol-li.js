28145a24f4dbb7d863d43d8930c0b4cf23bfce24
Shuvalov Anton <anton_shuvalov@me.com>
2014-03-21 00:44:11 +0600

    up

--- assets/js/ol-li.js
+++ assets/js/ol-li.js
@@ -1,14 +1,18 @@
 /* globals document */
-/**
- * Sorry, i'm so sorry...
- */
-var ols = document.querySelectorAll('ol');
-for (var ol in ols) {
-  if (ols.hasOwnProperty(ol) && ol !== 'length') { // WTF?
-    for (var li in ols[ol].children ) {
-      if (ols.hasOwnProperty(ol) && li !== 'length' && 'function' !== typeof ols[ol].children[li]) { // WTF?
-        ols[ol].children[li].innerHTML = '<span class="number">'+(li*1+1)+'. </span>'+ols[ol].children[li].innerHTML;
-      }
-    }
-  }
-}
+
+$('ol').each(function () {
+  $(this).find('li').each(function (k, i) {
+    var text = '<span class="number">%number. </span>';
+    $(this).prepend(text.replace('%number', k));
+  });
+});
+
+// for (var ol in ols) {
+//   if (ols.hasOwnProperty(ol) && ol !== 'length') { // WTF?
+//     for (var li in ols[ol].children ) {
+//       if (ols.hasOwnProperty(ol) && li !== 'length' && 'function' !== typeof ols[ol].children[li]) { // WTF?
+//         ols[ol].children[li].innerHTML = '<span class="number">'+(li*1+1)+'. </span>'+ols[ol].children[li].innerHTML;
+//       }
+//     }
+//   }
+// }
