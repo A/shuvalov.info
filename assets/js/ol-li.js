ef1aa725eda2b917ad785c358c62f1524425a6a6
Shuvalov Anton <anton_shuvalov@me.com>
2014-04-08 22:09:24 +0600

    update ipad markup

--- assets/js/ol-li.js
+++ assets/js/ol-li.js
@@ -3,6 +3,6 @@
 $('ol').each(function () {
   $(this).find('li').each(function (k, i) {
     var text = '<span class="number">%number. </span>';
-    $(this).prepend(text.replace('%number', k));
+    $(this).prepend(text.replace('%number', k+1));
   });
 });
